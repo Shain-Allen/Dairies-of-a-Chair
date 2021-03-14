3a3686f5de72f15ac9bf15347a4690c8f7eecf34
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-03-13 17:44:31 -0800

    prep for material gathering during week

---
+++ Javascript/PlayerInv.js
@@ -0,0 +1,12 @@
+/*window.PlayerInv = function (wood, plastic, spareWheel, paint) {
+    this.wood = wood || 0;
+    this.plastic = plastic || 0;
+    this.spareWheel = spareWheel || 0;
+    this.paint = paint || 0;
+};
+
+PlayerInv.prototype._init = function (obj) {
+    Object.keys(obj).forEach(function (pn) {
+        this[pn] = clone(obj[pn]);
+    }, this);
+}*/
