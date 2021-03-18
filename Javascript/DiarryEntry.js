efe2c4e30d3118ec44b056b508b8b13a08b49473
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-03-17 19:31:27 -0700

    progress on diarry

---
+++ Javascript/DiarryEntry.js
@@ -0,0 +1,25 @@
+window.DiarryEntry = function (playerText, gatheredItems, roomsVisted) {
+    this.playerText = playerText | "";
+    this.gatheredItems = gatheredItems;
+    this.roomsVisted = roomsVisted;
+};
+
+DiarryEntry.prototype._init = function (obj) {
+    Object.keys(obj).forEach(function (pn) {
+        this[pn] = clone(obj[pn]);
+    }, this);
+
+    return this;
+};
+
+DiarryEntry.prototype.clone = function () {
+    return (new DiarryEntry())._init(this);
+};
+
+DiarryEntry.prototype.toJSON = function () {
+    var owndata = {};
+    Object.keys(this).forEach(function (pn) {
+        owndata[pn] = clone(this[pn]);
+    }, this);
+    return JSON.reviveWrapper('(new DiarryEntry())._init($ReviveData$', owndata);
+};
